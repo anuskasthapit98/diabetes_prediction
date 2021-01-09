221f281a12c1f792d037158004d4412c591c090e
Anuska Sthapit <anuskasthapit@sxc.edu.np>
2021-01-09 19:22:10 +0545

    first

--- app.py
+++ app.py
@@ -19,11 +19,9 @@ def predict():
     text3 = request.form['3']
     text4 = request.form['4']
     text5 = request.form['5']
-    text6 = request.form['6']
-    text7 = request.form['7']
-    text8 = request.form['8']
+    
  
-    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8])])
+    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5])])
     print(row_df)
     prediction=model.predict_proba(row_df)
     output='{0:.{1}f}'.format(prediction[0][1], 2)
