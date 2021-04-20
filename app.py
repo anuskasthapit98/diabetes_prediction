ac10ed638ba97c001fb4ba3fe40e58c48edab750
Anuska Sthapit <anuskasthapit@sxc.edu.np>
2021-04-20 08:45:36 +0545

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-model = pickle.load(open("Diabetes1.pkl", "rb"))
+model = pickle.load(open("Diabetes_Final_RFC.pkl", "rb"))
 
 
 @app.route('/')
@@ -19,9 +19,12 @@ def predict():
     text3 = request.form['3']
     text4 = request.form['4']
     text5 = request.form['5']
+    text6 = request.form['6']
+    text7 = request.form['7']
+    text8 = request.form['8']
     
  
-    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5])])
+    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8])])
     print(row_df)
        
     
